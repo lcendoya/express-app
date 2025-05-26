06478709f85960492800c04b560a30f1b61ca7ef
Lucas Cendoya <lcendoya@hotmail.com>
2025-05-25 22:00:49 -0300

    change message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello, I am Lucas, the creator of express-app!</h1>')
 })
 
 const PORT = 8080
